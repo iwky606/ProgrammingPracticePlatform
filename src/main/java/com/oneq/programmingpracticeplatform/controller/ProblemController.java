09c73fbc1ca20032b91595993bc811b4ec147a46
wangkongyu <kongyu.wang@maxjia.com>
2024-04-07 21:53:04 +0800

    dev:create problem

--- src/main/java/com/oneq/programmingpracticeplatform/controller/ProblemController.java
+++ src/main/java/com/oneq/programmingpracticeplatform/controller/ProblemController.java
@@ -1,20 +1,27 @@
 package com.oneq.programmingpracticeplatform.controller;
 
+import com.oneq.programmingpracticeplatform.annotation.AuthCheck;
 import com.oneq.programmingpracticeplatform.common.BaseResponse;
 import com.oneq.programmingpracticeplatform.common.ResultUtils;
+import com.oneq.programmingpracticeplatform.model.enums.AuthEnum;
+import com.oneq.programmingpracticeplatform.service.ProblemService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/problem")
 public class ProblemController {
+    @Resource
+    ProblemService problemService;
 
     @PostMapping
-    public BaseResponse<Long> createProblem() {
-        Long id = null;
+    @AuthCheck(mustRole = AuthEnum.TEACHER)
+    public BaseResponse<Long> createProblem(HttpServletRequest request) {
+        long id = problemService.createProblem(request);
         return ResultUtils.success(id);
     }
 }

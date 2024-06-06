a2a4e2fb149d828f90100bd2fd92ead77ed34795
wangkongyu <kongyu.wang@maxjia.com>
2024-06-06 15:56:39 +0800

    lint

--- src/main/java/com/oneq/programmingpracticeplatform/model/vo/ProblemVo.java
+++ src/main/java/com/oneq/programmingpracticeplatform/model/vo/ProblemVo.java
@@ -1,12 +1,10 @@
 package com.oneq.programmingpracticeplatform.model.vo;
 
-import cn.hutool.json.JSONUtil;
 import com.oneq.programmingpracticeplatform.model.entity.problem.JudgeConfig;
 import com.oneq.programmingpracticeplatform.model.entity.problem.Problem;
 import com.oneq.programmingpracticeplatform.model.entity.problem.ProblemDescription;
 import com.oneq.programmingpracticeplatform.model.entity.problem.ProblemSolution;
 import com.oneq.programmingpracticeplatform.model.enums.ProblemVisibleEnum;
-import io.swagger.models.auth.In;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
